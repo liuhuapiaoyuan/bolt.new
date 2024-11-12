1b2419ced33c2c83d28399588424a29acce48ab3
liuhuapiaoyuan <278780765@qq.com>
2024-11-12 11:42:23 +0800

    feat: 增加docker配置

---
+++ docker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+# 修改镜像标签为当前日期时间
+time=$(date "+%Y%m%d%H%M%S")
+image=liuhuapiaoyuan/bolt.new
+# 构建镜像，指定Dockerfile以及Context
+docker build -t $image:$time -f docker/Dockerfile .
+docker tag $image:$time $image:latest
+docker push $image:latest
+docker push $image:$time
